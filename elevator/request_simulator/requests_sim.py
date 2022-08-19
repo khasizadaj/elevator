b056a5b963aeabe2319826ed34bf62ff7f4fde1a
Javid Khasizada <56765877+khasizadaj@users.noreply.github.com>
2022-08-19 22:49:13 +0200

    Revert "add: update/add methods to get request timely"

--- elevator/request_simulator/requests_sim.py
+++ elevator/request_simulator/requests_sim.py
@@ -8,26 +8,11 @@ GeneratorOfRequests = Generator[Request, None, None]
 class RequestSimulator:
     def __init__(self, requests: str) -> None:
         self.requests = requests
-        self.processed_requests = []
-        self.curr_request_pointer = 0
 
     def generate(self) -> GeneratorOfRequests:
         all_raw_requests = self.requests.replace(" ", "").split(",")
         for curr_request in all_raw_requests:
-            self.processed_requests.append(self.process(curr_request))
-
-    def next(self, time: int) -> list[Request]:
-        if self.curr_request_pointer >= len(self.processed_requests):
-            return None
-
-        requests = []
-        for request in self.processed_requests[self.curr_request_pointer :]:
-            if request.requested_time == time:
-                requests.append(request)
-
-        self.curr_request_pointer += len(requests)
-
-        return requests
+            yield self.process(curr_request)
 
     def process(self, request: str) -> Request:
         """
